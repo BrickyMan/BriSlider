dac0b4aac9ade39fa0f2c7c1c2aaa5c055355d94
Bricksman <bricks.man@yahoo.com>
2022-04-06 21:58:05 +0300

    Add sensor swipe

--- js/slider_swipe.js
+++ js/slider_swipe.js
@@ -1,20 +1,47 @@
 let sliderBody = document.querySelector('#sliderBody'),
-	beginTouch = 0;
+	pageBody = document.querySelector('body'),
+	beginTouch = 0,
+	isTouch = false;
 
 sliderBody.onmousedown = (event) => {
 	beginTouch = event.clientX;
+	isTouch = true;
 }
 
-sliderBody.onmouseup = (event) => {
-	let endTouch = event.clientX,
-		isSwipe = Math.abs(beginTouch - endTouch) > 250 ? true : false;
-	// console.log(beginTouch, endTouch);
-	if (beginTouch < endTouch && isSwipe) {
-		console.log('to the left');
-		switchPrev();
+sliderBody.ontouchstart = (event) => {
+	beginTouch = event.changedTouches[0].clientX;
+	isTouch = true;
+}
+
+pageBody.onmouseup = (event) => {
+	if (isTouch) {
+		let endTouch = event.clientX,
+			isSwipe = Math.abs(beginTouch - endTouch) > 250 ? true : false;
+		// console.log(beginTouch, endTouch);
+		if (beginTouch < endTouch && isSwipe) {
+			console.log('to the left');
+			switchPrev();
+		}
+		else if (beginTouch > endTouch && isSwipe) {
+			console.log('to the right');
+			switchNext();
+		}
+		isTouch = false;
 	}
-	else if (beginTouch > endTouch && isSwipe) {
-		console.log('to the right');
-		switchNext();
+}
+
+pageBody.ontouchend = (event) => {
+	if (isTouch) {
+		let endTouch = event.changedTouches[0].clientX,
+			isSwipe = Math.abs(beginTouch - endTouch) > 250 ? true : false;
+		if (beginTouch < endTouch && isSwipe) {
+			console.log('to the left');
+			switchPrev();
+		}
+		else if (beginTouch > endTouch && isSwipe) {
+			console.log('to the right');
+			switchNext();
+		}
+		isTouch = false;
 	}
 }
